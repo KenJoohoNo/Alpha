6a861cff30def0394c4cb0042441c075eb417b7a
Pyeonno <pyeonno13634@gmail.com>
2025-04-20 22:36:15 +0900

    브라우저 연동성 문제

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        'last 2 versions',
+        '> 1%',
+        'Firefox ESR',
+        'not dead',
+        'not IE 11'
+      ],
+      grid: true,
+      flexbox: true
+    },
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   css: {
     postcss: './postcss.config.js',
+    devSourcemap: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    minify: 'terser',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'],
+        },
+      },
+    },
   },
 })
